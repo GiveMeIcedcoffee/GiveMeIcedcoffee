591751cbf126a300035e0af1acfb2e1ce1226799
GiveMeIcedcoffee <altheaapostol001@gmail.com>
2025-04-28 23:48:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 function toggleTheme() {
-        const body = document.body;
-        body.classList.toggle('dark-mode');
-        const button = document.querySelector('.toggle-btn');
-        if (body.classList.contains('dark-mode')) {
-          button.textContent = '☀️ Light Mode';
-        } else {
-          button.textContent = '🌙 Dark Mode';
-        }
-      }
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+
+  const button = document.querySelector('.toggle-btn');
+  if (body.classList.contains('dark-mode')) {
+    button.textContent = '☀️ Light Mode';
+  } else {
+    button.textContent = '🌙 Dark Mode';
+  }
+}
